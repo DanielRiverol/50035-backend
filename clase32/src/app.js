ebb88b4a8277a48d544f117788584e19b95cc75d
Daniel Riverol <dannyriverol@hotmail.com>
2024-04-22 12:09:44 -0300

    Clase 32

--- clase32/src/app.js
+++ clase32/src/app.js
@@ -1,9 +1,26 @@
 import express from "express";
 import usersRouter from "./routes/user.router.js";
+import compression  from 'express-compression'
+
+import errorHandler from './middlewares/errors/index.js'
 const app = express();
 
 app.set("PORT", process.env.PORT || 3000);
 
+app.use(express.json())
+
 app.use("/api/users", usersRouter);
+app.use(errorHandler)
+//midd compression
+app.use(compression({brotli:{enabled:true,zlib:{}}}))
+//router para un string
+app.get("/string", (req, res) => {
+  let string = `Soy Homero el malo`;
+  for (let i = 0; i < 111000; i++) {
+    string += `Soy Homero el malo`;
+  }
+
+  res.send(string);
+});
 
 app.listen(app.get("PORT"), console.log(`Server on port ${app.get("PORT")}`));
--- clase32/src/routes/user.router.js
+++ clase32/src/routes/user.router.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { generateUser } from "../utils.js";
-
+import CustomError from "../services/errors/CustomError.js";
+import EErrors from "../services/errors/enums.js";
+import { generateErrorInfo } from "../services/errors/info.js";
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -9,6 +11,32 @@ router.get("/", async (req, res) => {
     users.push(generateUser());
   }
   res.send({ status: "success", payload: users });
+});
+
+//data
+let users = [];
+router.post("/", (req, res) => {
+  const { first_name, last_name, age, email } = req.body;
+  //manejo de errores
+
+  if (!first_name || !last_name || !email) {
+    CustomError.createError({
+      name: "Error creando el usuario",
+      cause: generateErrorInfo({ first_name, last_name, age, email }),
+      message: "Error al intentar crear el usuario",
+      code: EErrors.INVALID_TYPES_ERROR,
+    });
+  }
+
+  const user = { first_name, last_name, age, email };
+  if (users.length === 0) {
+    user.id = 1;
+  } else {
+    user.id = users[users.length - 1].id + 1;
+  }
+
+  users.push(user);
+  res.send({ status: "succes", payload: user });
 });
 
 export default router;
